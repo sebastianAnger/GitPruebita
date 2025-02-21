fa003d2959c25693e1f62c881ffa27a3519889d3
sebastianAnger <sebastiananger.95@gmail.com>
2025-02-21 13:03:37 -0700

    Add files via upload

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,185 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hola</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hola</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Choice</name>
+        <message>
+            <source>no/no nao/disagree/I disagree</source>
+            <comment>SpeechReco negative</comment>
+            <translation type="vanished">no/no nao/disagree/I disagree</translation>
+        </message>
+        <message>
+            <source>yes/yes nao/agree/I agree/of course</source>
+            <comment>SpeechReco positive</comment>
+            <translation type="vanished">yes/yes nao/agree/I agree/of course</translation>
+        </message>
+        <message>
+            <source>help/help me/i don't know</source>
+            <comment>SpeechReco help</comment>
+            <translation type="vanished">help/help me/i don't know</translation>
+        </message>
+        <message>
+            <source>exit/stop/quit</source>
+            <comment>SpeechReco exit</comment>
+            <translation type="vanished">exit/stop/quit</translation>
+        </message>
+        <message>
+            <source>repeat/pardon/what/excuse me</source>
+            <comment>SpeechReco repeat</comment>
+            <translation type="vanished">repeat/pardon/what/excuse me</translation>
+        </message>
+        <message>
+            <source>I understood %s. Is that correct?</source>
+            <comment>TTS confirmation</comment>
+            <translation type="vanished">I understood %s. Is that correct?</translation>
+        </message>
+        <message>
+            <source>, / or </source>
+            <comment>TTS enumMarks</comment>
+            <translation type="vanished">, / or </translation>
+        </message>
+        <message>
+            <source>No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</source>
+            <comment>TTS helpEnumChoices</comment>
+            <translation type="vanished">No answer is really expected. /Your answer can be %s. /Your answer can be for example %s. /%s?</translation>
+        </message>
+        <message>
+            <source> you can ask me: %s. </source>
+            <comment>TTS helpEnumDefault</comment>
+            <translation type="vanished"> you can ask me: %s. </translation>
+        </message>
+        <message>
+            <source>You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</source>
+            <comment>TTS helpTactile</comment>
+            <translation type="vanished">You can also use my tactile sensor to choose the answer./\Pau=600\ Press the front or the rear of my skull cap to go through all of the available answers. Press then the circle in the middle to validate your choice.</translation>
+        </message>
+        <message>
+            <source>I did not understand. </source>
+            <comment>TTS notUnderstood</comment>
+            <translation type="vanished">I did not understand. </translation>
+        </message>
+        <message>
+            <source>No question has been defined, so I cannot repeat it. </source>
+            <comment>TTS noQuestion</comment>
+            <translation type="vanished">No question has been defined, so I cannot repeat it. </translation>
+        </message>
+        <message>
+            <source>There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </source>
+            <comment>TTS notUnderstoodAnims</comment>
+            <translation type="vanished">There is too much noise here! /Try to talk to me more distinctly. /I can't hear you very well. </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Diagram/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>espere 3 segundos</source>
+            <comment>Text</comment>
+            <translation type="obsolete">espere 3 segundos</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Diagram/Say (1)</name>
+        <message>
+            <source>espere 5 segundos</source>
+            <comment>Text</comment>
+            <translation type="obsolete">espere 5 segundos</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Diagram/Say (2)</name>
+        <message>
+            <source>LISTO</source>
+            <comment>Text</comment>
+            <translation type="obsolete">LISTO</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Hola a todos</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hola a todos</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>NADA</source>
+            <comment>Text</comment>
+            <translation type="obsolete">NADA</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/hola</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+        <message>
+            <source>hola MArio</source>
+            <comment>Text</comment>
+            <translation type="obsolete">hola MArio</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>hola amigo</source>
+            <comment>Text</comment>
+            <translation type="unfinished">hola amigo</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/no</name>
+        <message>
+            <source>no</source>
+            <comment>Text</comment>
+            <translation type="obsolete">no</translation>
+        </message>
+        <message>
+            <source>no se</source>
+            <comment>Text</comment>
+            <translation type="obsolete">no se</translation>
+        </message>
+        <message>
+            <source>no que perro, no me hables asi</source>
+            <comment>Text</comment>
+            <translation type="obsolete">no que perro, no me hables asi</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/si</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+        <message>
+            <source>si</source>
+            <comment>Text</comment>
+            <translation type="obsolete">si</translation>
+        </message>
+    </context>
+</TS>
---
+++ translations/translation_es_ES.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+    <context>
+        <name>behavior_1/behavior.xar:/Diagram/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hola</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hola</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/hola</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hola</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>hola amigo</source>
+            <comment>Text</comment>
+            <translation type="unfinished"></translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/si</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hola</translation>
+        </message>
+    </context>
+</TS>
